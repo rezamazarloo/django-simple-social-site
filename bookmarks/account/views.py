32d25394297db8eaa9d3d9baaedcdd53e2d6cca8
reza <rezamazarloo@yahoo.com>
2023-03-25 19:28:48 +0430

    added user register

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegisterationForm
 
 
 # Create your views here.
@@ -25,6 +25,21 @@ def user_login(request):
         form = LoginForm()
     return render(request, "account/login.html", {"form":form})
 
+
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegisterationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user":new_user})
+    else:
+        user_form = UserRegisterationForm()
+    return render(request, "account/register.html", {"user_form":user_form})
+
