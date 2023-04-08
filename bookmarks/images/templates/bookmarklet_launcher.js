46628ba3cdd1d6e86c7e6532e7076905dad1a3f5
reza <rezamazarloo@yahoo.com>
2023-04-08 17:47:10 +0430

    added bookmarklet.js

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -1,14 +1,1 @@
-(function () {
-    if (!window.bookmarklet) {
-      bookmarklet_js = document.body.appendChild(
-        document.createElement("script")
-      );
-      bookmarklet_js.src =
-        "//mysite.com:8000/static/js/bookmarklet.js?r=" +
-        Math.floor(Math.random() * 9999999999999999);
-      window.bookmarklet = true;
-    } else {
-      bookmarkletLaunch();
-    }
-  })();
-  
+(function(){if(!window.bookmarklet){bookmarklet_js=document.body.appendChild(document.createElement('script'));bookmarklet_js.src='//mysite.com:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*9999999999999999);window.bookmarklet=true;}else{bookmarkletLaunch();}})();
