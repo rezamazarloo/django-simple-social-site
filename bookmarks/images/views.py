11b30155651f20d79f3879e503387fa44a393b1e
reza <rezamazarloo@yahoo.com>
2023-04-11 14:43:36 +0430

    added scroll image list pagination

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -47,4 +48,23 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
         return JsonResponse({"status":"error"})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse("")
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, "images/image/list_images.html", {"section":"images", "images":images})
+    return render(request, "images/image/list.html", {"section":"images", "images":images})
 
